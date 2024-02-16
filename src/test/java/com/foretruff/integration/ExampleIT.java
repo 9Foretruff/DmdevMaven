dc38b64d9c846f90d314dc0ed2ad23962ada5bbd
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-17 00:31:59 +0200

    lesson17

---
+++ src/test/java/com/foretruff/integration/ExampleIT.java
@@ -0,0 +1,14 @@
+package com.foretruff.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
