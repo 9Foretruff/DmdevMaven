b137364dd27be537ac46c423df1e56221d7ce997
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-18 01:43:33 +0200

    lesson 28

---
+++ hello-plugin/src/main/java/com/foretruff/HelloMojo.java
@@ -0,0 +1,17 @@
+package com.foretruff;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "hello")
+public class HelloMojo extends AbstractMojo {
+    @Parameter(defaultValue = "hello from plugin" , property = "hello.message")
+    private String message;
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info(message);
+    }
+}
